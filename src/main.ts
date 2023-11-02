e88ea159adcab31528272e3c0e0ba6528f632148
Joshua Widjaja <josh@widjaja.us>
2023-11-01 22:47:57 -0700

    Added deposit button

--- src/main.ts
+++ src/main.ts
@@ -64,13 +64,23 @@ function makePit(i: number, j: number) {
         const container = document.createElement("div");
         container.innerHTML = `
                 <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
-                <button id="collect">collect</button>`;
+                <button id="collect">collect</button>
+                <button id="deposit">deposit</button>`;
         const collect = container.querySelector<HTMLButtonElement>("#collect")!;
         collect.addEventListener("click", () => {
             if (value > 0) {
                 value--;
                 container.querySelector<HTMLSpanElement>("#value")!.innerHTML = value.toString();
                 points++;
+                statusPanel.innerHTML = `${points} points accumulated`;
+            }
+        });
+        const deposit = container.querySelector<HTMLButtonElement>("#deposit")!;
+        deposit.addEventListener("click", () => {
+            if (points > 0) {
+                value++;
+                container.querySelector<HTMLSpanElement>("#value")!.innerHTML = value.toString();
+                points--;
                 statusPanel.innerHTML = `${points} points accumulated`;
             }
         });
